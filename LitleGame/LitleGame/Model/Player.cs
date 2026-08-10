ab46e5d5c8aebb6697a21d4acf70587c36199ca6
BobrickCS22 <132747159+BobrickCS22@users.noreply.github.com>
2026-08-10 12:03:09 +0300

    .

---
+++ LitleGame/LitleGame/Model/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LitleGame.Model
+{
+    public class Player
+    {
+        string Nik_Name { get; set; }
+        int Points { get; set; }
+
+    }
+}
