60ff4839840e6c3805e4ba17713c1f575b9381e6
BobrickCS22 <132747159+BobrickCS22@users.noreply.github.com>
2026-08-11 14:30:34 +0300

    MovingBeta

--- LitleGame/LitleGame/GameField/Field.cs
+++ LitleGame/LitleGame/GameField/Field.cs
@@ -8,6 +8,70 @@ namespace LitleGame.GameField
 {
     public class Field
     {
+        public void PrintField()
+        {
+            string[,] Field = new string[10, 10];
 
+            for (int i = 0; i < Field.GetLength(0); i++)
+            {
+                for (int j = 0; j < Field.GetLength(1); j++)
+                {
+                    Field[i, j] = "-";
+                }
+            }
+
+            for (int i = 0; i < Field.GetLength(0); i++)
+            {
+                for (int j = 0; j < Field.GetLength(1); j++)
+                {
+                    Console.Write(Field[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void PlayerMovement()
+        {
+            string[,] Field = new string[10, 10];
+            int playerX = 5;
+            int playerY = 0;
+            ConsoleKeyInfo keyInfo;
+            keyInfo = Console.ReadKey(true);
+            while (true)
+            {
+                Console.Clear();
+                Field[playerY, playerX] = "-";
+                for (int i = 0; i < Field.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Field.GetLength(1); j++)
+                    {
+                        if (keyInfo.Key == ConsoleKey.LeftArrow)
+                        {
+                            if (playerX > 0)
+                            {
+                                playerX--;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.RightArrow)
+                        {
+                            if (playerX < Field.GetLength(1) - 1)
+                            {
+                                playerX++;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < Field.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Field.GetLength(1); j++)
+                    {
+                        Console.Write(Field[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                keyInfo = Console.ReadKey(true);
+            }
+        }
     }
 }
--- LitleGame/LitleGame/Program.cs
+++ LitleGame/LitleGame/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LitleGame.GameField;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,8 @@ namespace LitleGame
     {
         static void Main(string[] args)
         {
-
+           Field field = new Field();
+            field.PlayerMovement();
         }
     }
 }
